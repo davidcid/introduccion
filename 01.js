9c53cf278cbfe6dc8d8d18d96686c82e8426cd4b
David Cid <david.cid@gmail.com>
2019-06-23 01:54:29 +0200

    Lesson 5

--- 01.js
+++ 01.js
@@ -1,30 +1,9 @@
 const app = new Vue({
   el: '#app',
   data: {
-    titulo: 'Hola mundo con Vue',
-    frutas: [
-      {nombre:'Pera', cantidad: 10},
-      {nombre:'Manzana', cantidad: 0},
-      {nombre:'Platano', cantidad: 11}
-    ],
-    nuevaFruta: '',
-    total: 0
+    fondo: 'bg-warning',
+    color: true
   },
   methods: {
-    agregarFruta() {
-      this.frutas.push({
-        nombre: this.nuevaFruta, cantidad: 0
-      })
-      this.nuevaFruta = '';
-    }
-  },
-  computed: {
-    sumarFrutas() {
-      this.total = 0
-      for(fruta of this.frutas) {
-        this.total = this.total + fruta.cantidad
-      }
-      return this.total
-    }
   }
 })
