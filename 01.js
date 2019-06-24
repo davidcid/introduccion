d7d00699103579d314040191dfd9d54f1c5d51db
David Cid <david.cid@gmail.com>
2019-06-24 22:33:59 +0200

    Componentes básicos

--- 01.js
+++ 01.js
@@ -1,17 +1,3 @@
 const app = new Vue({
-  el: '#app',
-  data: {
-    saludo: "Soy el ciclo de vida de Vue"
-  },
-  methods: {
-    destruir() {
-      this.$destroy();
-    }
-  },
-  computed: {
-
-  },
-  beforeCreate() {
-    console.log('beforeCreate')
-  }
+  el: '#app'
 })
